30e23a67a6dce06843a4b75f5a4abfdc4061438c
YannineSaavedra <162144036+YannineSaavedra@users.noreply.github.com>
2024-06-07 22:22:05 -0400

    Prueba 1

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <set>
+
+using namespace std;
+
+void respuesta(string fecha, string evento, string comando);
+
+void add_repetidos(map<string, set<string>> mapa, string fecha, string evento);
