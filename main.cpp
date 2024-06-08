bddcf43c22317e3d8127d66ec03fae2825e266ca
YannineSaavedra <162144036+YannineSaavedra@users.noreply.github.com>
2024-06-07 22:32:26 -0400

    Prueba 9

--- main.cpp
+++ main.cpp
@@ -15,3 +15,4 @@ void delOne(map<string, set<string>> mapa, string fecha, string evento);
 void find(map<string, set<string>> mapa, string fecha);
 void print( const map<string, set<string>> mapa);
 bool comprobar_dia(int dia);
+bool comprobar_mes(int mes);
