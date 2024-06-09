26955bbd5b2e290083a12320d9329d569911ab53
YannineSaavedra <162144036+YannineSaavedra@users.noreply.github.com>
2024-06-08 23:03:40 -0400

    Declaracion de la funcion y estructura

--- main.cpp
+++ main.cpp
@@ -1,109 +1,14 @@
 #include <iostream>
 #include <map>
-#include <string>
 #include <set>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-void respuesta(string fecha, string evento, string comando);
+// Declaración de la función SplitString
+vector<string> SplitString(const string& str, char delimiter);
 
-void add_repetidos(map<string, set<string>> mapa, string fecha, string evento);
-void add_repetidos(map<string, set<string>> mapa, string fecha, string evento);
-void add(map<string, set<string>> mapa, string fecha, string evento);
-void del(map<string, set<string>> mapa, string fecha);
-void delOne(map<string, set<string>> mapa, string fecha, string evento);
-void find(map<string, set<string>> mapa, string fecha);
-void print( const map<string, set<string>> mapa);
-bool comprobar_dia(int dia);
-bool comprobar_mes(int mes);
-
-int main()
-{
-
-    string comando, fecha, evento;
-    do {
-        cout<<"Comando: "<<endl;
-        cin >> comando;
-        cout<<"Fecha y Evento: "<<endl;
-        cin >> fecha >> evento;
-        respuesta(fecha, evento, comando);
-    } while (comando != "x");
-     return 0;
-}
-void respuesta(string fecha, string evento, string comando){
-     map<string, set<string>> eventos;
-     if( comando == "Add") {
-        if (eventos.count(fecha) != 0){
-            add_repetidos(eventos, fecha, evento);
-        }
-        else {
-            add(eventos, fecha, evento); //anhadir evento
-        }
-    }
-    else if( comando == "Del") {
-        if(evento != ""){ //eliminar un solo evento
-        delOne(eventos, fecha, evento);
-        } else{ //eliminar todos los eventos de una fecha
-            del(eventos, fecha);
-        }
-    }
-    else if( comando == "Find") {
-        if (eventos.count(fecha) != 0){
-            find(eventos, fecha); //encontrar eventos por fecha
-        }
-        else {
-            "No existe";
-        }
- 
-    }
-    else if ( comando ==  "Print") {
-        print(eventos); //mostrar todos los eventos
-    }
-}
-
-void add_repetidos(map<string, set<string>> mapa, string fecha, string evento){
-    set<string> eventos = mapa.find(fecha)->second;
-    eventos.insert(evento);
-    mapa[fecha] = eventos;
-}
-//anhadir eventos por fecha si no existen
-void add(map<string, set<string>> mapa, string fecha, string evento){
-    set<string> eventos;
-    eventos.insert(evento);
-    mapa[fecha] = eventos;
-}
-//encontrar todos los eventos por fecha
-void find(map<string, set<string>> mapa, string fecha){
-    set<string> eventos = mapa.find(fecha)->second; //guardar todos los eventos de la fecha en un set
-for (auto e : eventos){
-        cout<<fecha<<" : "<<e<<endl; //muestra todos los eventos de la fecha
-    }
-}
-//eliminar todos los eventos de una fecha
-void del(map<string, set<string>> mapa, string fecha){
-
-}
-//eliminar un solo evento especifico
-void delOne(map<string, set<string>> mapa, string fecha, string evento){
-
-}
-//mostrar todos los eventos
-void print( const map<string, set<string>> mapa){
-    for (auto item : mapa){
-        set<string> eventos = item.second;
-        for (auto e : eventos){
-        cout<<item.first<<" : "<<e<<endl; //muestra todos los eventos de la fecha
-        }
-    }
-}
-//comprobar que el dia este en el rango correcto
-bool comprobar_dia(int dia){
-    if (dia > 0 && dia <= 31){return true;}
-    return false;
-}
-//comprobar que el mes este en el rango correcto
-bool comprobar_mes(int mes){
-    if (mes > 0 && mes <= 12){return true;}
-    return false;
-}
-            
+// Estructura de datos para almacenar los eventos por fecha
+struct Database {
+    map<string, set<string>> data;
