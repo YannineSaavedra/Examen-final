2304a9d8b90b0be35b3ed23e115040cebcd157f2
YannineSaavedra <162144036+YannineSaavedra@users.noreply.github.com>
2024-06-17 09:19:33 -0400

    HEADER

--- main.cpp
+++ main.cpp
@@ -1,74 +1,48 @@
 #include "headerPrincipal.h"
-#include "funciones.h"
 
-struct FECHA {
-    int año;
-    int mes;
-    int dia;
-    };
-
-int main() {
-    string comando, fecha, evento;
-    map<string, set<string>> FechaEventos;
-    FECHA f;
-    
-    while(true){
-    cin >> comando;
-    if(comando == "exit") break;
-    if(comando.empty()) continue;
-    if(comando == "Print"){
-        print(FechaEventos);
-        continue;
+vector<int> stovector_fecha(string fecha){
+    string s;
+    vector<int> v;
+    int counter_hypens = 0;
+    bool negative = false;
+    for (int i = 0; i < fecha.size(); ++i) {
+    if (fecha[i] == '-') {
+    ++counter_hypens;
+    if (!s.empty()) {
+        v.push_back(stoi(s));
+        s = "";
+    }
+    negative = true;
+    } else if (fecha[i] == '+') {
+        if (!s.empty()) {
+            v.push_back(stoi(s));
+            s = "";
+        }
+    negative = false;
+    } else {
+        s += fecha[i];
+    }
+    }
+    if (!s.empty()) {
+        v.push_back(stoi(s));
     }
     
-    if(comando != "Add" && comando != "Del" && comando != "Find" && comando != "Print"){
-        cout << "Unknown command: " << comando << endl;
-        continue;
-    }
-    
-    cin >> fecha;
-    vector<int> vi = stovector_fecha(fecha);
-    if(vi.size() != 3){
+    if(counter_hypens >= 4){
         cout << "Wrong date format: " << fecha << endl;
-        continue;
     }
-    
-        f.año = vi[0];
-        f.mes = vi[1];
-        f.dia = vi[2];
-    
-    if(comprobar_mes(f.mes) == false) {
-        cout << "Month value is invalid: " + to_string(f.mes) << endl;
-        continue;
+    return v;
+}
+
+bool comprobar_mes(int mes){
+    if(mes >= 1 && mes <= 12){
+        return true;
     }
-    if(comprobar_dia(f.dia) == false) {
-        cout << "Day value is invalid: " + to_string(f.dia) << endl;
-        continue;
-        }
-    
-    cin >> evento;
-    if(comando == "Add"){
-        add(fecha, evento, FechaEventos);
-    } else if(comando == "Del"){
-        if(!evento.empty()){
-            del_one(fecha, evento, FechaEventos);
-            /*if(FechaEventos[fecha].erase(evento)){
-                cout << "Deleted successfully" << endl;
-            } else {
-            cout << "Event not found" << endl;
-        }*/
-        } else {
-            del(fecha, FechaEventos); //Falta el mensaje
-        }
-    } else if(comando == "Find"){
-        if (FechaEventos.count(fecha) != 0) {
-                find(fecha, FechaEventos);
-            } else {
-                cout << "No existe" << endl;
-            } //se imprime despues del comando no después.
+    return false;
+}
+
+bool comprobar_dia(int dia){
+    if(dia >= 1 && dia <= 31){
+        return true;
     }
-    
-    
-    }
-    return 0;
+    return false;
 }
