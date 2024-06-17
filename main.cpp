338d2203a2ce116abf55c7f487b232f233121d66
YannineSaavedra <162144036+YannineSaavedra@users.noreply.github.com>
2024-06-17 09:20:02 -0400

    FUNCIONES

--- main.cpp
+++ main.cpp
@@ -1,48 +1,33 @@
 #include "headerPrincipal.h"
 
-vector<int> stovector_fecha(string fecha){
-    string s;
-    vector<int> v;
-    int counter_hypens = 0;
-    bool negative = false;
-    for (int i = 0; i < fecha.size(); ++i) {
-    if (fecha[i] == '-') {
-    ++counter_hypens;
-    if (!s.empty()) {
-        v.push_back(stoi(s));
-        s = "";
-    }
-    negative = true;
-    } else if (fecha[i] == '+') {
-        if (!s.empty()) {
-            v.push_back(stoi(s));
-            s = "";
+void add(string fecha, string evento, map<string, set<string>>& FechaEventos){
+    FechaEventos[fecha].insert(evento);
+}
+
+void del_one(string fecha, string evento, map<string, set<string>>& FechaEventos){
+    if(FechaEventos.count(fecha)){
+        if(FechaEventos[fecha].empty()){
+            FechaEventos.erase(fecha);
         }
-    negative = false;
-    } else {
-        s += fecha[i];
-    }
-    }
-    if (!s.empty()) {
-        v.push_back(stoi(s));
-    }
-    
-    if(counter_hypens >= 4){
-        cout << "Wrong date format: " << fecha << endl;
-    }
-    return v;
+    } 
 }
 
-bool comprobar_mes(int mes){
-    if(mes >= 1 && mes <= 12){
-        return true;
-    }
-    return false;
+void del(string fecha, map<string, set<string>>& FechaEventos){
+    FechaEventos.erase(fecha);
 }
 
-bool comprobar_dia(int dia){
-    if(dia >= 1 && dia <= 31){
-        return true;
+void find(string fecha, map<string, set<string>>& FechaEventos){
+    set<string> eventos = FechaEventos[fecha];
+    for (const auto& e : eventos) {
+        cout << e << endl;
     }
-    return false;
+}
+
+void print(const map<string, set<string>>& FechaEventos){
+    for (const auto& item : FechaEventos) {
+        const set<string>& eventos = item.second;
+        for (const auto& e : eventos) {
+            cout << item.first << " : " << e << endl;
+        }
+    }
 }
